b90a808dc0831daae94ff81187c912627e0452cc
An Quach <quachan888@gmail.com>
2022-02-09 03:40:47 -0500

    Update Hero photo dynamic fetch images

--- src/components/Header/Hero.js
+++ src/components/Header/Hero.js
@@ -1,32 +1,18 @@
-import React from "react";
-import {
-    MDBCarousel,
-    MDBCarouselInner,
-    MDBCarouselItem,
-    MDBCarouselElement,
-} from "mdb-react-ui-kit";
+import React from 'react';
+import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBCarouselElement } from 'mdb-react-ui-kit';
 
 export default function Hero() {
     return (
         <MDBCarousel showIndicators showControls className="mb-4 shadow">
-            <MDBCarouselInner style={{ height: 300 }}>
+            <MDBCarouselInner style={{ height: 400 }}>
                 <MDBCarouselItem className="active">
-                    <MDBCarouselElement
-                        src="https://source.unsplash.com/HONJP8DyiSM"
-                        alt="..."
-                    />
+                    <MDBCarouselElement src="https://source.unsplash.com/1600x900/?store" alt="..." />
                 </MDBCarouselItem>
                 <MDBCarouselItem>
-                    <MDBCarouselElement
-                        src="https://source.unsplash.com/tMI2_-r5Nfo"
-                        alt="..."
-                    />
+                    <MDBCarouselElement src="https://source.unsplash.com/1600x900/?reactjs" alt="..." />
                 </MDBCarouselItem>
                 <MDBCarouselItem>
-                    <MDBCarouselElement
-                        src="https://source.unsplash.com/SYTO3xs06fU"
-                        alt="..."
-                    />
+                    <MDBCarouselElement src="https://source.unsplash.com/1600x900/?ecommerce" alt="..." />
                 </MDBCarouselItem>
             </MDBCarouselInner>
         </MDBCarousel>
